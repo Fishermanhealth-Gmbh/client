99eaa7a7a759edd6efc6c7ae81e0b411d392525e
vyaire backend <ole.zierau@peix.de>
2021-01-13 11:12:33 +0100

    added reload menue point for prod

--- app/menu.ts
+++ app/menu.ts
@@ -131,6 +131,14 @@ export default class MenuBuilder {
       label: 'View',
       submenu: [
         {
+          label: 'Reload',
+          accelerator: 'Command+R',
+          click: () => {
+            this.mainWindow.webContents.reload();
+            this.mainWindow.reload();
+          }
+        },
+        {
           label: 'Toggle Full Screen',
           accelerator: 'Ctrl+Command+F',
           click: () => {
